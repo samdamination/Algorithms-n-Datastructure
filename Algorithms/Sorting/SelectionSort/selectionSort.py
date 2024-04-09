e9d466e008bf680c94b26b2ea71d44cc73793421
Samuele Daminato <samuele.daminato@usi.ch>
2024-04-09 18:06:06 +0200

    finished selectionSort

--- Algorithms/Sorting/SelectionSort/selectionSort.py
+++ Algorithms/Sorting/SelectionSort/selectionSort.py
@@ -3,5 +3,29 @@
 ########################################
 
 ## INTRO ##
-# Complexity:
+# Complexity: O(n^2)
+#      worst: O(n^2)
+#      best: O(n^2)
+#      average: O(n^2)
 
+
+def selectionSort(A):
+    #
+    for i in range(len(A) - 1):
+        min = i
+        for j in range (i + 1, len(A)):
+            if A[j] < A[min]:
+                min = j
+        A[i], A[min] = A[min], A[i]
+        # print(A) # to check intermediate steps
+    return A
+
+
+# versione bibbia identica
+
+
+
+### TESTS ###
+A = [5, 6, 12, 8, 4, 10, 3, 12, 11, 1]
+
+print(selectionSort(A))
