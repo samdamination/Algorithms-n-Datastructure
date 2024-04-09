b7a101106a509cc75fa8052025ebedf8bc2db6c4
Samuele Daminato <samuele.daminato@usi.ch>
2024-04-09 16:06:49 +0200

    finished bubbleSort, insertionSort

--- Algorithms/Sorting/BubbleSort/bubbleSort.py
+++ Algorithms/Sorting/BubbleSort/bubbleSort.py
@@ -1,11 +1,14 @@
 ########################################
-###    BINARY SEARCH - Algorithm     ###
+###    BUBBLE SORT - Algorithm     ###
 ########################################
 
 ## INTRO ##
 # Complexity: O(n^2)
+#      worst: O(n^2)
+#      best: flag, O(n)
+#      average: O(n^2)
 
-# Compara a coppie e trasporta il numero maggiore in fondo
+# Compara a coppie e trasporta i numeri minori in cima
 # (se la coppia è fuori posizione li swappa). A ogni nuova iteration
 # la parte finale dell'array è sempre sorted
 # in questo caso si parte dal fondo
@@ -13,16 +16,30 @@
 
 
 def bubbleSort(A):
-    for i in range(len(A)):
-        for j in range(i + 1, len(A)):
-            if A[j] < A[j - 1]:
-                A[j], A[j - 1] = A[j - 1], A[j]
+    # n is the number of elements of A
+    # for n-1 passes
+    for i in range(len(A) - 1):
+        # compare each pair so n-1-i (number of comparisons minus one every pass)
+        for j in range(len(A) - 1 - i):
+            # if left > right swap
+            if A[j] > A[j + 1]:
+                A[j], A[j + 1] = A[j + 1], A[j]
             #print(A) # to see intermediate results
     return A
+
+# version that sorts from end also possible
+# versione bibbia
+""" def bubbleSort(A):
+    for i in range(len(A)):
+        for j in range(len(A) - 1, i, -1):
+            if A[j] < A[j - 1]:
+                A[j], A[j - 1] = A[j - 1], A[j]
+                print(A)
+    return A """
 
 
 
 ## TESTS ##
 A = [5, 6, 12, 8, 4, 10, 3, 12, 11, 1]
 
-bubbleSort(A)
+print(bubbleSort(A))
---
+++ Algorithms/Sorting/InsertionSort/insertionSort.py
@@ -0,0 +1,49 @@
+########################################
+###    INSERTION SORT - Algorithm    ###
+########################################
+
+## INTRO ##
+# Complexity: O(n^2)
+#      worst: reverse order, O(n^2)
+#      best: already sorted, linear O(n)
+#      average: O(n^2)
+
+
+def insertionSort(A):
+    # per tutta la parte di array non sortato (il primo elem. lo e)
+    for i in range(1, len(A)):
+        # j tiene traccia dell'ultimo elemento dell'array sortato (all'inizio è i-1)
+        j = i - 1
+        # salviamo come temp il primo elem. dell'array non sortato, che sarà da inserire
+        x = A[i]
+        # quando l'elem. dell'array sortato (j) è maggiore di x crea spazio e diminuisci j
+        # j deve essere > -1 se indice array complessivo parte da 0 ot. out of bound
+        while j > -1 and A[j] > x:
+            A[j + 1] = A[j]
+            j = j - 1
+        # fuori dal while = j indica elem. minore di x
+        # copia x in j + 1
+        A[j + 1] = x
+    return A
+
+
+
+# versione bibbia
+""" def insertionSort(A):
+	i = 1
+	while i < len(A):
+		k = i
+		while k > 0 and A[k] < A[k - 1]:
+			A[k], A[k - 1] = A[k - 1], A[k] # swap
+			print(A)
+			k -= 1
+		i += 1
+	return A """
+
+
+
+### TESTS ###
+A = [5, 6, 12, 8, 4, 10, 3, 12, 11, 1]
+
+print(insertionSort(A))
+    
