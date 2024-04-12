971ecd919e33b5739d10733a13748fb2bdd42004
Samuele Daminato <samuele.daminato@usi.ch>
2024-04-12 15:27:09 +0200

    added stack.py and pseudocode

---
+++ DataStructures/Stack/stack.py
@@ -0,0 +1,36 @@
+####### STACK - Datastructure #######
+# --------------------------------- #
+
+# INTRO: straightforward, see theory
+
+# DATASTRUCTURE
+# creating the stack S to store info and variable N for meta-data (size)
+S = [None]*100
+N = 0
+
+# push
+def push(x):
+    global S, N
+    if N < len(S):
+        S[N] = x
+        N = N + 1
+    else:
+        print('stack overflow')
+
+# pop
+def pop():
+    global S, N
+    if N > 0:
+        N = N - 1
+        return S[N]
+    else:
+        print('stack underflow')
+
+# isEmpty
+def is_empty():
+    global S, N
+    return N == 0
+
+
+
+
